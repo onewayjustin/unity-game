37ee1d2f7c40fbff611e31f38a6b5595b4bb9846
onewayjustin <150301983+onewayjustin@users.noreply.github.com>
2024-02-21 17:50:23 -0500

    Update playermovement.cs

--- playermovement.cs
+++ playermovement.cs
@@ -4,32 +4,46 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-public float speed;
-public float jump;
+    public float speed;
+    public float jump;
 
-private float move;
+    private float Move;
 
-public Rigidbody2D rb;
+    public Rigidbody2D rb;
 
+   public bool isJumping;
+   
     // Start is called before the first frame update
-void Start()
+    void Start()
     {
         
     }
 
-    // Update is called once per frame
-void Update()
-{
-    
-    move=input.getaxis("Horizont");
+        // Update is called once per frame
+    void Update()
+    {
+        
+        Move = Input.GetAxis("Horizontal");
 
-    rb.velocity=new Vector2(speed * Move, rb.velocity.y);
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
 
-    if (input. getButtonDown("jump")) {
-        rb.AddfForce(newVector2(rb. velocity. x, jump));
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
 
+
+
+
+private void onCollisionEnter2D(Collision2D other)
+        }
+     if(other.gameObject.CompareTag("Floor"))
+
+        isJumping= flase;
     }
+}
 
-    
-}}
+private void onCollisionExit2D(Collision2D other)
+
+if(other.gameObject.CompareTag("Floor"))
+
+    isJumping =true;
 
